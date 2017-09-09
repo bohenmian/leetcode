a6966dea9c4a205f2f9646c1b3e647b41c24bc01
bohenmian <398206896@qq.com>
2017-09-09 11:38:13 +0800

    update test for border

--- src/test/java/com/nowcoder/recursion/JumpFloorTest.java
+++ src/test/java/com/nowcoder/recursion/JumpFloorTest.java
@@ -10,10 +10,25 @@ public class JumpFloorTest {
     private JumpFloor jumpFloor = new JumpFloor();
 
     @Test
-    public void jumpFloor() throws Exception {
+    public void test_jumpFloor_when_target_more_than_2() throws Exception {
         int target = 4;
         int result = jumpFloor.JumpFloor(target);
         assertThat(result, is(5));
     }
+
+    @Test
+    public void test_jumpFloor_when_target_less_than_0() throws Exception {
+        int target = -1;
+        int result = jumpFloor.JumpFloor(target);
+        assertThat(result, is(-1));
+    }
+
+    @Test
+    public void test_jumpFloor_when_target_in_2() throws Exception {
+        int target = 1;
+        int result = jumpFloor.JumpFloor(target);
+        assertThat(result, is(target));
+    }
+
 
 }
