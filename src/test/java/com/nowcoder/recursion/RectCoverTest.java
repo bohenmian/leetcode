0003b3c950984d27f3c33661e83154baf8b9bfdb
bohenmian <398206896@qq.com>
2017-09-09 11:59:54 +0800

    Accepted

---
+++ src/test/java/com/nowcoder/recursion/RectCoverTest.java
@@ -0,0 +1,18 @@
+package com.nowcoder.recursion;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RectCoverTest {
+    private RectCover rectCover = new RectCover();
+
+    @Test
+    public void rectCover() throws Exception {
+        int target = 3;
+        int result = rectCover.RectCover(target);
+        assertThat(result, is(3));
+    }
+
+}
