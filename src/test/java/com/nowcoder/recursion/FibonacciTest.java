6dc89d7dfd7cebec4ad803aef9518d319b34f2c5
bohenmian <398206896@qq.com>
2017-09-09 11:22:39 +0800

    Accepted

---
+++ src/test/java/com/nowcoder/recursion/FibonacciTest.java
@@ -0,0 +1,18 @@
+package com.nowcoder.recursion;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+    private Fibonacci fibonacci = new Fibonacci();
+
+    @Test
+    public void fibonacci() throws Exception {
+        int n = 6;
+        int result = fibonacci.Fibonacci(n);
+        assertThat(result, is(13));
+    }
+
+}
