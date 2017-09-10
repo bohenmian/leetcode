c5a319057c79e6ba02e0aa53bfd3bcde84eb8a72
bohenmian <398206896@qq.com>
2017-09-10 10:01:31 +0800

    Accepted

---
+++ src/main/java/com/nowcoder/common/Power.java
@@ -0,0 +1,30 @@
+package com.nowcoder.common;
+
+/**
+ * question
+ * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方
+ */
+
+public class Power {
+    public double Power(double base, int exponent) {
+        double sum = 1.0;
+        if (exponent == 0) {
+            return 1.000000;
+        }
+        if (base > -0.000001 && base < 0.000001) {
+            return 0;
+        }
+        if (exponent > 0) {
+            for (int i = 0; i < exponent; i++) {
+                sum = sum * base;
+            }
+        } else {
+            exponent = -exponent;
+            for (int i = 0; i < exponent; i++) {
+                sum = sum * base;
+            }
+            sum = 1.0 / sum;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/com/nowcoder/common/PowerTest.java
@@ -0,0 +1,35 @@
+package com.nowcoder.common;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PowerTest {
+    private Power power = new Power();
+
+    @Test
+    public void test_power_when_base_is_0() throws Exception {
+        double base = 0;
+        int exponent = 10;
+        double result = power.Power(base, exponent);
+        assertThat(result, is(Double.valueOf(0)));
+    }
+
+    @Test
+    public void test_power_when_exponent_is_1() throws Exception {
+        double base = 10;
+        int exponent = 1;
+        double result = power.Power(base, exponent);
+        assertThat(result, is(Double.valueOf(10)));
+    }
+
+    @Test
+    public void test_power_when_base_is_2_and_exponent_is_5() throws Exception {
+        double base = 2;
+        int exponent = 5;
+        double result = power.Power(base, exponent);
+        assertThat(result, is(Double.valueOf(32)));
+    }
+
+}
