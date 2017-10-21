dff6b72cfff4d3951c4666608b5c6cfefee71c8a
bohenmian <398206896@qq.com>
2017-10-21 20:41:48 +0800

    add test

---
+++ src/test/java/com/nowcoder/common/movingCount/MoveCountTest.java
@@ -0,0 +1,21 @@
+package com.nowcoder.common.movingCount;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoveCountTest {
+
+    private MoveCount moveCount = new MoveCount();
+
+    @Test
+    public void moveCount() throws Exception {
+        int rows = 10;
+        int cols = 10;
+        int threshold = 5;
+        int result = moveCount.movingCount(threshold, rows, cols);
+        assertEquals(result, 21);
+
+    }
+
+}
