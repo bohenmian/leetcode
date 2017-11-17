478b90dff3c332229f9a9545802b1aa61efc839c
bohenmian <398206896@qq.com>
2017-11-17 19:33:54 +0800

    fix exception

--- src/main/java/com/nowcoder/array/ReOrderArray.java
+++ src/main/java/com/nowcoder/array/ReOrderArray.java
@@ -1,7 +1,7 @@
 package com.nowcoder.array;
 
 /**
- * question 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+ * question 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分
  */
 
 public class ReOrderArray {
@@ -19,9 +19,9 @@ public class ReOrderArray {
             while (i < j && (array[j] & 1) == 0) {
                 j--;
             }
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
         }
         return array;
     }
