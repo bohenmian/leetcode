fe723108aebe41463a6be88263b111a92f04f3be
bohenmian <398206896@qq.com>
2017-11-17 19:58:07 +0800

    Accepted

--- src/main/java/com/nowcoder/list/findKthToTail/FindKthToTail.java
+++ src/main/java/com/nowcoder/list/findKthToTail/FindKthToTail.java
@@ -1,5 +1,9 @@
 package com.nowcoder.list.findKthToTail;
 
+/**
+ * question 输入一个链表，输出该链表中倒数第k个结点。
+ */
+
 public class FindKthToTail {
     public ListNode FindKthToTail(ListNode head, int k) {
         if (head == null || k <= 0) {
