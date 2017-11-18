35ca89e68d64ee1501127ab4f6195828a828c987
bohenmian <398206896@qq.com>
2017-11-18 10:35:01 +0800

    add test

--- src/test/java/com/nowcoder/list/mergeList/MergeTest.java
+++ src/test/java/com/nowcoder/list/mergeList/MergeTest.java
@@ -2,6 +2,7 @@ package com.nowcoder.list.mergeList;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class MergeTest {
@@ -21,5 +22,22 @@ public class MergeTest {
         ListNode listNode2 = null;
         ListNode result = merge.merge(listNode1, listNode2);
         assertEquals(result, null);
+    }
+
+    @Test
+    public void merge() throws Exception {
+        int i = 1;
+        ListNode listNode1 = new ListNode(1);
+        ListNode node1 = new ListNode(3);
+        ListNode listNode2 = new ListNode(2);
+        ListNode node2 = new ListNode(4);
+        listNode1.next = node1;
+        listNode2.next = node2;
+        ListNode result = merge.merge(listNode1, listNode2);
+        while (result != null) {
+            assertEquals(result.val, i);
+            i++;
+            result = result.next;
+        }
     }
 }
