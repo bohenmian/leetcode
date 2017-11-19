30a5fb38194e57b95fb87a6d68be6fbfcbd70210
bohenmian <398206896@qq.com>
2017-11-19 17:22:36 +0800

    add test

--- src/test/java/com/nowcoder/common/CountNumberOfOneTest.java
+++ src/test/java/com/nowcoder/common/CountNumberOfOneTest.java
@@ -11,14 +11,14 @@ public class CountNumberOfOneTest {
     @Test
     public void test_nubberOf1_when_number_is_0() throws Exception {
         int n = 0;
-        int result = countNumberOfOne.NubberOf1(n);
+        int result = countNumberOfOne.NumberOf1(n);
         assertEquals(result, 0);
     }
 
     @Test
     public void test_nubberOf1_when_number_is_15() throws Exception {
         int n = 15;
-        int result = countNumberOfOne.NubberOf1(n);
+        int result = countNumberOfOne.NumberOf1(n);
         assertEquals(result, 4);
     }
 
