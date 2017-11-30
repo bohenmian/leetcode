a745f2b56734c0a99fdb2de78db8f9ef983ee213
bohenmian <398206896@qq.com>
2017-11-30 21:12:22 +0800

    Accepted

--- src/main/java/com/nowcoder/string/Permutation.java
+++ src/main/java/com/nowcoder/string/Permutation.java
@@ -1,10 +1,13 @@
 package com.nowcoder.string;
 
-import java.util.ArrayList;
+
+/**
+ * * 题目描述：输入一个字符串，打印出该字符串中字符的所有排列。例如输入字符串abc，
+ *           则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab,cba.
+ */
 
 public class Permutation {
     public  static void Permutation(String string) {
-        ArrayList<String> strings = new ArrayList<>();
         if (string == null) {
             return;
         }
