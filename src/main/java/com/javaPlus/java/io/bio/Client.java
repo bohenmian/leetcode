1733a71e708092e3c4f4d2d1a5e656a977ea2d8f
bohenmian <bohenmian@gmail.com>
2018-04-22 16:44:56 +0800

    BIO Demo

---
+++ src/main/java/com/javaPlus/java/io/bio/Client.java
@@ -0,0 +1,36 @@
+package com.javaPlus.java.io.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    private static final int PORT = 8080;
+    private static final String MESSAGE = "Client Data";
+    private static final String LOCALHOST = "127.0.0.1";
+
+    public static void main(String[] args) {
+        try {
+            //监听本机8080端口
+            Socket socket = new Socket(LOCALHOST, PORT);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            //向服务端写数据
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            printWriter.print(MESSAGE);
+            printWriter.flush();
+
+            //接收服务端返回的数据
+            String line = reader.readLine();
+            System.out.println(line);
+
+            reader.close();
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/javaPlus/java/io/bio/Server.java
@@ -0,0 +1,34 @@
+package com.javaPlus.java.io.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) {
+        try {
+            //服务端监听8080端口
+            ServerSocket server = new ServerSocket(PORT);
+            //接收客户端的请求
+            Socket socket = server.accept();
+
+            //用来接收Client发送过来的消息
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //打印出Client发送给服务端的消息
+            String line = reader.readLine();
+            System.out.println("received data from client" + " " + line);
+
+            reader.close();
+            socket.close();
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
