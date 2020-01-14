1339eab2abc25313b0ae7eda1cfe7d19534ced56
bohenmian <bohenmian@gmail.com>
2020-01-14 17:26:20 +0800

    finish find number with even number of digits

--- src/main/java/com/leetcode/array/FindNumbersWithEvenNumberOfDigits.java
+++ src/main/java/com/leetcode/array/FindNumbersWithEvenNumberOfDigits.java
@@ -6,4 +6,13 @@ public class FindNumbersWithEvenNumberOfDigits {
     public int findNumbers(int[] nums) {
         return (int) Arrays.stream(nums).filter(num -> String.valueOf(num).length() % 2 == 0).count();
     }
+
+    public int findNumbersInOtherWay(int[] nums) {
+        int result = 0;
+        for (int num : nums)
+            if ((num >= 10 && num < 100) || (num >= 1000 && num < 10000) || num == 100000) {
+                result++;
+            }
+        return result;
+    }
 }
---
+++ src/test/java/com/leetcode/array/FindNumbersWithEvenNumberOfDigitsTest.java
@@ -0,0 +1,24 @@
+package com.leetcode.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindNumbersWithEvenNumberOfDigitsTest {
+
+    private FindNumbersWithEvenNumberOfDigits test = new FindNumbersWithEvenNumberOfDigits();
+
+    @Test
+    public void should_return_the_even_number_of_digits() {
+        int[] numbers = new int[]{12, 345, 2, 6, 7896};
+        int result = test.findNumbers(numbers);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void should_return_the_even_number_of_digits_with_two_test() {
+        int[] numbers = new int[]{555, 901, 482, 1771};
+        int result = test.findNumbers(numbers);
+        assertEquals(1, result);
+    }
+}
