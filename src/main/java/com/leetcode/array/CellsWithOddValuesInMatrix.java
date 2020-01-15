9f5cae8b3fadb48d045181cdb18b04fcb303dc82
bohenmian <bohenmian@gmail.com>
2020-01-15 16:25:02 +0800

    finish find cells with pdd values in matrix

--- src/main/java/com/leetcode/array/CellsWithOddValuesInMatrix.java
+++ src/main/java/com/leetcode/array/CellsWithOddValuesInMatrix.java
@@ -1,4 +1,39 @@
 package com.leetcode.array;
 
-public class CellswithOddValuesinMatrix {
+import java.util.Arrays;
+
+public class CellsWithOddValuesInMatrix {
+
+    public int oddCells(int n, int m, int[][] indices) {
+        int[][] oddMatrix = new int[n][m];
+        for (int[] ind : indices) {
+            for (int col = 0; col < m; col++) {
+                oddMatrix[ind[0]][col]++;
+            }
+            for (int row = 0; row < n; row++) {
+                oddMatrix[row][ind[1]]++;
+            }
+        }
+        return (int) Arrays.stream(oddMatrix)
+                .flatMapToInt(Arrays::stream)
+                .filter(value -> value % 2 == 1)
+                .count();
+    }
+
+    public int oddCellsInOtherWays(int n, int m, int[][] indices) {
+        int result = 0;
+        boolean[] col = new boolean[n];
+        boolean[] row = new boolean[m];
+        for (int[] ind : indices) {
+            col[ind[0]] ^= true;
+            row[ind[1]] ^= true;
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (col[i] ^ row[j])
+                    result++;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/com/leetcode/array/CellsWithOddValuesInMatrixTest.java
+++ src/test/java/com/leetcode/array/CellsWithOddValuesInMatrixTest.java
@@ -1,5 +1,24 @@
-import static org.junit.Assert.*;
+package com.leetcode.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CellsWithOddValuesInMatrixTest {
 
+    private CellsWithOddValuesInMatrix matrix = new CellsWithOddValuesInMatrix();
+
+    @Test
+    public void should_return_the_number_of_cells_with_odd_values() {
+        int[][] indices = {{0, 1}, {1, 1}};
+        int result = matrix.oddCells(2, 3, indices);
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void should_return_the_number_of_cells_with_odd_values_in_other_ways() {
+        int[][] indices = {{1, 1}, {0, 0}};
+        int result = matrix.oddCellsInOtherWays(2, 2, indices);
+        assertEquals(0, result);
+    }
 }
